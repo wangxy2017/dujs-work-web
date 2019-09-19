63873f550ae325476f83d60e9f4615b0969e03b5
wxy <wangxiaoyuan@pand-auto.com>
2019-09-19 15:56:46 +0800

    code

--- src/main/java/com/wxy/util/ApiResponse.java
+++ src/main/java/com/wxy/util/ApiResponse.java
@@ -13,23 +13,23 @@ public class ApiResponse {
     private String msg;
     private Object data;
 
-    public ApiResponse(Integer code, String msg, Object data) {
+    private ApiResponse(Integer code, String msg, Object data) {
         this.code = code;
         this.msg = msg;
         this.data = data;
     }
 
-    private static final Integer DEFAULT_SUCCESS_CODE = 1;
-    private static final String DEFAULT_SUCCESS_MSG = "SUCCESS";
-    private static final Integer DEFAULT_ERROR_CODE = -1;
-    private static final String DEFAULT_ERROR_MSG = "ERROR";
+    private static final Integer SUCCESS_CODE = 1;
+    private static final String SUCCESS_MSG = "SUCCESS";
+    private static final Integer ERROR_CODE = -1;
+    private static final String ERROR_MSG = "ERROR";
 
     public static ApiResponse success() {
-        return new ApiResponse(DEFAULT_SUCCESS_CODE, DEFAULT_SUCCESS_MSG, null);
+        return new ApiResponse(SUCCESS_CODE, SUCCESS_MSG, null);
     }
 
     public static ApiResponse success(Object data) {
-        return new ApiResponse(DEFAULT_SUCCESS_CODE, DEFAULT_SUCCESS_MSG, data);
+        return new ApiResponse(SUCCESS_CODE, SUCCESS_MSG, data);
     }
 
     public static ApiResponse success(Integer code, String msg) {
@@ -41,7 +41,7 @@ public class ApiResponse {
     }
 
     public static ApiResponse error() {
-        return new ApiResponse(DEFAULT_ERROR_CODE, DEFAULT_ERROR_MSG, null);
+        return new ApiResponse(ERROR_CODE, ERROR_MSG, null);
     }
 
     public static ApiResponse error(Integer code, String msg) {
