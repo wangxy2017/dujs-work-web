0faf085d9c1b60815c898e383642483bd2ccda30
wxy <wangxiaoyuan@pand-auto.com>
2019-09-24 13:39:22 +0800

    拦截器

--- src/main/java/com/wxy/configuration/WebMvcConfig.java
+++ src/main/java/com/wxy/configuration/WebMvcConfig.java
@@ -12,14 +12,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // 日志拦截
+        registry.addInterceptor(new LoggingInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
+        // 登录拦截
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login/**", "/register/**")
                 .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**")
                 .excludePathPatterns("/bookmark/download/**");
-        registry.addInterceptor(new LoggingInterceptor())
-                .addPathPatterns("/**")
-                .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
     }
 
     @Override
